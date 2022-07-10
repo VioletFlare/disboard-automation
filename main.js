632c1c2f98a7dbf47255710f2e63dd9228fd4dd8
violetflare@protonmail.com <violetflare@protonmail.com>
2022-07-10 17:09:00 +0200

    setup

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
-const { fork } = require("child_process");
 const process = require("process");
-const path = require('path');
+const path = require("path");
+const cluster = require("cluster");
 
-const cucumberProgram = path.join(process.cwd(), 'node_modules', '@cucumber', 'cucumber', 'bin', 'cucumber-js');
-const cucumberConfJs = path.join(process.cwd(), "cucumber.conf.js");
-const stepsPath = path.join(process.cwd(), "steps");
-const features = path.join(process.cwd(), "features");
+class Main {
+  constructor() {
+    this.cucumberProgram = path.join(
+      __dirname, "node_modules", "@cucumber", "cucumber", "bin", "cucumber-js"
+    );
+    this.cucumberConfJs = path.join(__dirname, "cucumber.conf.js");
+    this.stepsPath = path.join(__dirname, "steps");
+    this.features = path.join(__dirname, "features");
+    this.args = [
+        "--require", this.cucumberConfJs, "--require", this.stepsPath, "--", this.features
+    ];
+  }
 
-const args = ["--require", cucumberConfJs, "--require", stepsPath, "--", features];
-fork(cucumberProgram, args);
+  init() {
+    if (cluster.isWorker && process.env.source) {
+
+    }
+
+    if (cluster.isPrimary) {
+      cluster.setupPrimary({
+        exec: this.cucumberProgram,
+        args: this.args
+      });
+      const worker = cluster.fork();
+
+      worker.on("exit", async (code) => {
+        console.debug(`[run] process finished with exit code`, code);
+      });
+    }
+  }
+}
+
+new Main().init();
